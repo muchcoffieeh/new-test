8b7eb6d7fe6a4a4e7e7cad47ee999644b30073f1
Serena <serena.xu@aaf.com>
2023-07-15 18:52:35 -0400

    logo image finally works

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import PostCard from "../components/postCard"
-import Image from "../components/image"
+import { StaticImage } from "gatsby-plugin-image"
+// import Image from "../components/image"
 
 import "../utils/normalize.css"
 import "../utils/css/screen.css"
@@ -42,12 +43,20 @@ const BlogIndex = () => {
   return (
     <Layout title={data.site.siteMetadata.title}>
       <header className="page-head">
-          {/* <img className="mainLogo" src={`sx-logo.png`} /> */}
-          <Image />
+          <StaticImage
+            src="../images/sx-logo.png"
+            loading="eager"
+            width={100}
+            quality={95}
+            formats={["auto", "webp", "avif"]}
+            alt=""
+            style={{ marginBottom: `var(--space-3)` }}
+          />
           <h1 className="page-head-title">Designed By Serena</h1>
 
           <h4 style={{ margin: "0.1em 0", fontSize: "1.4em" }}>
-            {data.site.siteMetadata.description}
+            {/* {data.site.siteMetadata.description} */}
+            Product Designer. Product Thinker. Passion for emerging tech.
           </h4>
         </header>
         {<div className="post-feed">
