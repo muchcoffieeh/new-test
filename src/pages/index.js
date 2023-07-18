9240190d2957166185cbc36ae3ba3691401a441a
Serena <serena.xu@aaf.com>
2023-07-18 16:41:42 -0400

    style groups

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ const BlogIndex = () => {
           <StaticImage
             src="../images/sx-logo.png"
             loading="eager"
-            width={100}
+            width={150}
             quality={95}
             formats={["auto", "webp", "avif"]}
             alt=""
@@ -63,15 +63,20 @@ const BlogIndex = () => {
         <div className="groups-container">
   <div className="group-container">
     <div className="group-content">
-      <h2>Collaboration</h2>
-      <p>dfalkdjglkadjgl</p>
+      <div className="vert-divider"></div>
+      <h2>Systems Thinking</h2>
+      <p>I lean in and deeply analyze the complicated webs of relationships between technical components, people, pain points, and market factors to inform my design decisions.</p>
+      <ul>
+        <li>Market Research</li>
+        <li>Competitive/Comparative Analysis</li>
+        <li>Persona Classification/Definition</li>
+      </ul>
     </div>
-    <div className="vert-divider"></div>
     <div className="group-image">
       <StaticImage
         src="../images/systems-icon.png"
         loading="eager"
-        width={100}
+        width={120}
         quality={95}
         formats={["auto", "webp", "avif"]}
         alt=""
@@ -94,15 +99,25 @@ const BlogIndex = () => {
     </div>
     <div className="vert-divider"></div>
     <div className="group-content">
-      <h2>Systems Thinking</h2>
-      <p>dfalkdjglkadjgl</p>
+      <h2>Collaboration</h2>
+      <p>Collaboration with cross-functional teams is my go-to for generating ideas and insights that give way to delightful customer experiences.</p>
+      <ul className="ul-reverse">
+        <li>Moderated/Unmoderated Interviews</li>
+        <li>Participatory Design Workshops</li>
+        <li>Information Architecture</li>
+      </ul>
     </div>
   </div>
 
   <div className="group-container">
     <div className="group-content">
-      <h2>Efficient Processes</h2>
-      <p>dfalkdjglkadjgl</p>
+      <h2>Process Efficiency</h2>
+      <p>Operate with a tried and true framework of research, conceptualize, and execute to deliver on key objectives while adjusting for the unique characteristics and constraints of each project.</p>
+      <ul>
+        <li>Product Requirements</li>
+        <li>User Flow Iteration</li>
+        <li>Prototype Testing</li>
+      </ul>
     </div>
     <div className="vert-divider"></div>
     <div className="group-image">
@@ -120,10 +135,12 @@ const BlogIndex = () => {
 </div>
 
 
-
-          <a href="https://www.linkedin.com/in/serenaxu/">See How I Operate</a>
+          <div className="groups-a">
+            <a href="https://www.linkedin.com/in/serenaxu/">See How I Operate</a>
+          </div>
         </div>
         <hr></hr>
+        <h2 className="section-h2">Case Studies</h2>
         {<div className="post-feed">
           {posts.map(post => {
             postCounter++
