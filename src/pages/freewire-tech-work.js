2476b43e73655c36d1642c52ec44724f3b353e02
Serena <serena.s.xu@gmail.com>
2023-08-06 10:39:55 -0700

    added header nav to comp pages

--- src/pages/freewire-tech-work.js
+++ src/pages/freewire-tech-work.js
@@ -6,6 +6,7 @@ import "../utils/normalize.css"
 import "../utils/css/screen.css"
 
 const SubpageA = ({ data }) => {
+  const [toggleNav, setToggleNav] = React.useState(false)
   const postsInCategoryA = data.allMdx.nodes.filter(
     (post) => post.frontmatter.category === "freewire-tech-work"
   );
@@ -49,6 +50,43 @@ const SubpageA = ({ data }) => {
       {isPasswordCorrect ? (
         <>
           <header className="page-head">
+          <div className="site-head-container">
+              <a
+                className="nav-burger"
+                href={`#`}
+                onClick={() => setToggleNav(!toggleNav)}
+              >
+                <div
+                  className="hamburger hamburger--collapse"
+                  aria-label="Menu"
+                  role="button"
+                  aria-controls="navigation"
+                >
+                  <div className="hamburger-box">
+                    <div className="hamburger-inner" />
+                  </div>
+                </div>
+              </a>
+              <nav id="swup" className="site-head-left">
+                <ul className="nav" role="menu">
+                  <li className="nav-home nav-current" role="menuitem">
+                    <Link to={`/`}>Home</Link>
+                  </li>
+                  <li className="nav-about" role="menuitem">
+                    <Link to={`#my-process`}>Process</Link>
+                  </li>
+                  <li className="nav-about" role="menuitem">
+                    <Link to={`#case-studies`}>Work</Link>
+                  </li>
+                  
+                  <li className="nav-elements" role="menuitem">
+                    <Link to={`https://www.linkedin.com/in/serena-s-xu/`} target="_blank">About</Link>
+                </li>
+                </ul>
+              </nav>
+              <div className="site-head-center"></div>
+            </div>
+            
         <h1 className="comp-head-title">FreeWire Technologies</h1>
 
         <h4 style={{ margin: "0.1em 0", fontSize: "1.4em" }}>
