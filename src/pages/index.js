a63b6fa57837629510be00c0a7c578320049c423
Serena <serena.s.xu@gmail.com>
2023-09-12 14:21:31 -0700

    fixed pantheon page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const sections = [
   {
   title: "Pantheon Platform",
     image: "../images/pantheon-comp.jpg",
-    link: "/pantheon-work/content/pantheon-edge/",
+    link: "/pantheon-work",
     text: {
       title: "WORK",
       content: (
